5c4f9eee23ae711e94f01efc2ac81efc987ee051
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-06-16 09:19:11 -0400

    add new addons
    -add decal0 plugin
    -add followbone3d plugin
    -create bullet holes when bullet impacts surfaces

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -1,15 +1,14 @@
 extends RigidBody
 
-
+var has_created_hole = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -19,3 +18,12 @@ func _ready():
 func _on_BulletCleanUpTimer_timeout():
 	queue_free() # Replace with function body.
 	#pass
+
+
+func _on_Bullet_body_entered(body):
+	if has_created_hole == false and body.is_in_group("enemies") == false:
+		var new_bullet_hole = load("res://scenes/BulletHole.tscn").instance()
+		get_tree().current_scene.add_child(new_bullet_hole)
+		new_bullet_hole.global_transform = transform
+		new_bullet_hole.global_scale(Vector3(.07,.07,.07))
+		has_created_hole = true # Replace with function body.
